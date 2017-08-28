1c1a64e728c5caad5462dd5186a0c7f284a4fbc5
Evan Kysley <evan@kysley.com>
2017-08-28 02:37:13 -0400

    DAY 4: SUBSCRIPTIONS WORK

--- src/js/components/ShowPoll.jsx
+++ src/js/components/ShowPoll.jsx
@@ -3,17 +3,71 @@ import { withRouter } from 'react-router-dom'
 import { graphql, compose } from 'react-apollo'
 import gql from 'graphql-tag'
 
+const allVotes = gql`
+query {
+  Poll(id: "cj6mtnsgkufou0183q5e6u9jo") {
+    id
+      options {
+        name
+        _votesMeta {
+          count
+        }
+      }
+    }
+  }
+`
+
 class ShowPoll extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
       value: '',
+      id: 'cj6mtnsgkufou0183q5e6u9jo',
     }
   }
 
-  componentWillMount() {
-    console.log("it will mount")
+  componentDidMount() {
+    console.log(allVotes)
+    console.log(this.props)
+    this.voteSubscription = this.props.allVotesQuery.subscribeToMore({
+      document: gql`
+      subscription watchVotes{
+        Vote(filter:
+        {
+          mutation_in: [CREATED]
+          node: {
+            option: {
+              poll: {
+                 id: "${this.state.id}"
+              }
+            }
+        }}
+        ) {
+          mutation
+            node {
+                option {
+                    _votesMeta {
+                      count
+                    }
+                  }
+                }
+              }
+            }
+      `,
+      variables: null,
+      updateQuery: (previousState, { subscriptionData }) => {
+        if (subscriptionData.data) {
+          console.log(subscriptionData.data)
+        }
+      },
+    })
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.VoteSub) {
+      console.log(nextProps)
+    }
   }
 
   render() {
@@ -25,4 +79,6 @@ class ShowPoll extends React.Component {
   }
 }
 
-export default withRouter(ShowPoll)
+const VoteSub = graphql(allVotes, { name: 'allVotesQuery' })(ShowPoll)
+
+export default withRouter(VoteSub)
