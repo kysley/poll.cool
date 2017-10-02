63ac7225226746a286be02d9abcb9079a34cde89
Evan Kysley <evan@kysley.com>
2017-10-02 17:02:25 -0400

    +loading banner

---
+++ src/js/components/LoadingModal.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import classNames from 'classnames'
+
+const LoadingModal = (props) => {
+  const classes = classNames({
+    'loading--alert': true,
+    'dur-3': true,
+    'fadeIn': props.active === true,
+    'fadeOut': props.active !== true,
+  })
+  return (
+    <div className={classes}>
+      <h1>Hang tight!</h1>
+      <h2>Our recordkeepers are prepping your Poll in record time.</h2>
+    </div>
+  )
+}
+
+export default LoadingModal
